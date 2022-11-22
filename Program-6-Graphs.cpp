f6abc17242bfeff8ad31e6b86ba0153a75179f73
Kevin Diaz <Kevindiaz1616@gmail.com>
2022-11-21 23:45:44 -0600

    new

---
+++ Program-6-Graphs.cpp
@@ -0,0 +1,57 @@
+/**************************
+ *
+ * HEADER
+ *
+ * ************************/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+//************************** Node *************************
+
+class Node;
+
+//************************** Graph ****************************
+class Graph {
+	public:
+		//add the node with neighborId as the neighbor of nodeId
+		void addNeighbor(int nodeId, int neighborId);
+
+		//reads the edge list from file and creates the adjacency list data structure
+		void loadGraph(string edgeListFileName);
+		
+		//writes the adjacency list into the file
+		void dumpGraph(string adjListFileName);
+
+		//Prints number of nodes, number of edges, and maximum degree on terminal
+		void printGraphInfo();
+
+		//returns the number of neighbor (degree) of a node
+		int getNumNeighbors(int nodeId);
+
+		//returns the number of nodes in the graph
+		int getNumVertices();
+
+	private:
+		vector<Node*> vertices;
+		
+};
+
+
+void run(string edgeListFileName, string adjListFileName) {
+}
+
+//*****************************************************************************
+
+
+// The main will be removed for testing, do not add any code in the main function 
+int main() {
+	//Change the filenames according to your local path.
+    string edgeListFileName("karate.txt");
+    string adjListFileName("karate_adj.txt");
+	run(edgeListFileName, adjListFileName);
+    return 0;
+}
